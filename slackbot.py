e36915ede98309c18c59f42e7294ea410be134ff
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-07-01 00:50:09 +0900

    hello, start text

--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import os
 import time
+import random
 from slackclient import SlackClient
 
 
@@ -9,7 +10,11 @@ BOT_ID = "U632C7TJT"
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
+HELLO_COMMAND = ["hi", "hello", "하이", "ㅎㅇ", "안녕", "안녕하세요", "안뇽"]
+HELLO_RETURN = ["hi", "hello", "하이", "ㅎㅇ", "안녕", "안녕하세요", "안뇽", "ㅇㅇ", "왜?", "OK퀴즈 풀래?", "ㅇㅋ"]
+START_TEXT = ["quiz", "game", "게임", "OX", "ox", "퀴즈", "내봐", "문제"]
+
+END_TEXT =
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient('xoxb-207080265639-QdACLVqTI3U3xQdJl8MXYCsk')
@@ -21,10 +26,14 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+    response = "미안해요.. 반나절만에 만든거라 다른 말은 대답 못해요. OX퀴즈나 풀어봅시다!"
+
+    for word in command:
+        if word.lower() in HELLO_COMMAND:
+            response = random.choice(HELLO_RETURN)
+        elif word.lower() in START_TEXT:
+            response = "게임을 시작하지.."
+
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
