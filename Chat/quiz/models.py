9b0f2f25fad35974adafe4b0150c06bf3b3b0044
yoonkt200 <yoonkt200@gmail.com>
2017-07-01 12:43:28 +0900

    model add

--- Chat/quiz/models.py
+++ Chat/quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(TimeStampedModel):
+    quiz = models.TextField(default="")
+    answer = models.CharField(max_length=200, default="")
+
+    def __str__(self):
+        return self.quiz
--- Chat/user/models.py
+++ Chat/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(TimeStampedModel):
+    slackid = models.CharField(max_length=200, default="")
+    context = models.CharField(max_length=200, default="")
+    quizNum = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.slackid
+
+    def nextQuiz(self):
+        self.quizNum = self.quizNum + 1
+        self.save()
